04eb54fbc6f01939385dae6597262fa0df50dd5a
Zulqurnain Haider <zulqurnain@dev.sdsol.com>
2017-10-17 20:31:56 +0500

    Update OnDateClickListener.java

--- zcalenderviewextended/src/main/java/jutt/com/zcalenderviewextended/week/listener/OnDateClickListener.java
+++ zcalenderviewextended/src/main/java/jutt/com/zcalenderviewextended/week/listener/OnDateClickListener.java
@@ -2,9 +2,6 @@ package jutt.com.zcalenderviewextended.week.listener;
 
 import org.joda.time.DateTime;
 
-/**
- * Created by nor on 12/5/2015.
- */
 public interface OnDateClickListener {
      void onDateClick(DateTime dateTime);
 }
