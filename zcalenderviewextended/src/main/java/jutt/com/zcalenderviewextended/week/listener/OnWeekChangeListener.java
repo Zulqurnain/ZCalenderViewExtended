3311afc8addf942c14ecb79f52480701c19c465d
Zulqurnain Haider <zulqurnain@dev.sdsol.com>
2017-10-17 20:32:17 +0500

    Update OnWeekChangeListener.java

--- zcalenderviewextended/src/main/java/jutt/com/zcalenderviewextended/week/listener/OnWeekChangeListener.java
+++ zcalenderviewextended/src/main/java/jutt/com/zcalenderviewextended/week/listener/OnWeekChangeListener.java
@@ -2,9 +2,6 @@ package jutt.com.zcalenderviewextended.week.listener;
 
 import org.joda.time.DateTime;
 
-/**
- * Created by gokhan on 7/28/16.
- */
 public interface OnWeekChangeListener {
 
     void onWeekChange(DateTime firstDayOfTheWeek, boolean forward);
