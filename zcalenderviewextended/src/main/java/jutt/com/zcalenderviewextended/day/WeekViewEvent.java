8f6a37dfcb67ef79739585825896e08c3436a9e8
Zulqurnain Haider <zulqurnain@dev.sdsol.com>
2017-10-17 20:25:27 +0500

    Update WeekViewEvent.java

--- zcalenderviewextended/src/main/java/jutt/com/zcalenderviewextended/day/WeekViewEvent.java
+++ zcalenderviewextended/src/main/java/jutt/com/zcalenderviewextended/day/WeekViewEvent.java
@@ -6,10 +6,7 @@ import java.util.List;
 
 import static jutt.com.zcalenderviewextended.day.WeekViewUtil.*;
 
-/**
- * Created by Raquib-ul-Alam Kanak on 7/21/2014.
- * Website: http://april-shower.com
- */
+
 public class WeekViewEvent {
     private long mId;
     private Calendar mStartTime;
